c4599b21e349a3f48a706e8dfc0b362ce0ec3d92
Gabriel Almeida <113075816+1neskk@users.noreply.github.com>
2023-03-07 11:14:18 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,334 @@
+#include <iostream>
+#include "api/KeyAuth.hpp"
+#include "xorstr.hpp"
+//#include <tlhelp32.h>
+#include <fstream>
+#include <filesystem>
+#include <iostream>
+#include <windows.h>
+#include <thread>
+#include <random>
+//#include <tchar.h>
+
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx9.h"
+
+
+
+#include <d3d9.h>
+#pragma comment(lib,"d3d9.lib")
+
+#pragma comment(lib, "D3dx9")
+
+//#define _CRT_SECURE_NO_WARNINGS
+
+IDirect3DTexture9* masterlogo;
+
+using namespace KeyAuth;
+
+std::string name = XorStr("Nesk"); //Application name found in application settings
+std::string ownerid = XorStr("GWIw5rI0kI"); //Owner ID Found in user settings
+std::string secret = XorStr("b4b6ffaa937d80aaa0603737c0b404d5e7b2d0a79a0c6c76cf9c71c9a3235757"); //Application secret found in Application settings
+std::string version = XorStr("1.0"); // Version can be changed but is not really important
+
+api KeyAuthApp(name, ownerid, secret, version);
+
+bool loader_active = true;
+
+static int width = 350;
+static int height = 200;
+
+char PassWord[20] = "";
+char Licence[50] = "";
+char UserName[20] = "";
+char RgPassWord[20] = "";
+char RgUserName[20] = "";
+
+HWND main_hwnd = nullptr;
+
+LPDIRECT3DDEVICE9        g_pd3dDevice;
+D3DPRESENT_PARAMETERS    g_d3dpp;
+LPDIRECT3D9              g_pD3D;
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+bool CreateDeviceD3D(HWND hWnd)
+{
+	if ((g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)) == NULL)
+		return false;
+
+	// Create the D3DDevice
+	ZeroMemory(&g_d3dpp, sizeof(g_d3dpp));
+	g_d3dpp.Windowed = TRUE;
+	g_d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	g_d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	g_d3dpp.EnableAutoDepthStencil = TRUE;
+	g_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+	g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;           // Present with vsync
+	//g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;   // Present without vsync, maximum unthrottled framerate
+	if (g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3dDevice) < 0)
+		return false;
+	return true;
+}
+
+void CleanupDeviceD3D()
+{
+	if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
+	if (g_pD3D) { g_pD3D->Release(); g_pD3D = NULL; }
+}
+
+void ResetDevice()
+{
+	ImGui_ImplDX9_InvalidateDeviceObjects();
+	HRESULT hr = g_pd3dDevice->Reset(&g_d3dpp);
+	if (hr == D3DERR_INVALIDCALL)
+		IM_ASSERT(0);
+	ImGui_ImplDX9_CreateDeviceObjects();
+}
+
+bool LoginCheck = false;
+
+void InfLog() {
+	RECT screen_rect;
+	GetWindowRect(GetDesktopWindow(), &screen_rect);
+	auto x = float(screen_rect.right - width) / 2.f;
+	auto y = float(screen_rect.bottom - width) / 2.f;
+
+	ImGui::SetNextWindowPos(ImVec2(x, y), ImGuiCond_Once);
+	ImGui::SetNextWindowSize(ImVec2(200, 70));
+	ImGui::Begin("More user info!", &loader_active, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoScrollbar);
+	{
+
+		ImGui::Columns(1, NULL, true);
+		{
+			ImGui::Text("Username: %s", KeyAuthApp.user_data.username);
+
+			time_t currtime;
+			struct tm* tminfo;
+			time(&currtime);
+			tminfo = localtime(&currtime); //If you get an error here, uncomment line 24
+
+			std::time_t x = std::mktime(tminfo);
+			std::time_t y = std::mktime(&KeyAuthApp.user_data.expiry);
+			if (x != (std::time_t)(-1) && y != (std::time_t)(-1))
+			{
+				double difference = std::difftime(y, x) / (60 * 60 * 24);
+				ImGui::Text("Day(s) left: %d", difference);
+			} 
+
+			ImGui::EndChild();
+		}
+	}
+	ImGui::End();
+}
+
+INT APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
+	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, L"Nesk", NULL };
+	RegisterClassEx(&wc);
+	main_hwnd = CreateWindow(wc.lpszClassName, L"Nesk", WS_POPUP, 0, 0, 5, 5, NULL, NULL, wc.hInstance, NULL);
+
+	if (!CreateDeviceD3D(main_hwnd))
+	{
+		CleanupDeviceD3D();
+		UnregisterClass(wc.lpszClassName, wc.hInstance);
+		return 1;
+	}
+	ShowWindow(main_hwnd, SW_HIDE);
+	UpdateWindow(main_hwnd);
+
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.IniFilename = nullptr;
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+	constexpr auto ColorFromBytes = [](uint8_t r, uint8_t g, uint8_t b)
+	{
+		return ImVec4((float)r / 255.0f, (float)g / 255.0f, (float)b / 255.0f, 1.0f);
+	};
+
+	auto& style = ImGui::GetStyle();
+	ImVec4* colors = style.Colors;
+
+	colors[ImGuiCol_Text] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+	colors[ImGuiCol_TextDisabled] = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+	colors[ImGuiCol_WindowBg] = ImVec4(0.15f, 0.15f, 0.15f, 0.94f);
+	colors[ImGuiCol_ChildBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+	colors[ImGuiCol_PopupBg] = ImVec4(0.08f, 0.08f, 0.08f, 0.94f);
+	colors[ImGuiCol_Border] = ImVec4(0.43f, 0.43f, 0.50f, 0.50f);
+	colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+	colors[ImGuiCol_FrameBg] = ImVec4(0.16f, 0.29f, 0.48f, 0.54f);
+	colors[ImGuiCol_FrameBgHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
+	colors[ImGuiCol_FrameBgActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
+	colors[ImGuiCol_TitleBg] = ImVec4(0.07f, 0.07f, 0.09f, 1.00f);
+	colors[ImGuiCol_TitleBgActive] = ImVec4(0.07f, 0.07f, 0.09f, 1.00f);
+	colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.07f, 0.07f, 0.09f, 0.51f);
+	colors[ImGuiCol_MenuBarBg] = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+	colors[ImGuiCol_ScrollbarBg] = ImVec4(0.02f, 0.02f, 0.02f, 0.53f);
+	colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.31f, 0.31f, 0.31f, 1.00f);
+	colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.41f, 0.41f, 0.41f, 1.00f);
+	colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.51f, 0.51f, 0.51f, 1.00f);
+	colors[ImGuiCol_CheckMark] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	colors[ImGuiCol_SliderGrab] = ImVec4(0.24f, 0.52f, 0.88f, 1.00f);
+	colors[ImGuiCol_Button] = ImVec4(0.36f, 0.36f, 0.36f, 0.40f);
+	colors[ImGuiCol_ButtonHovered] = ImVec4(0.61f, 0.61f, 0.59f, 1.00f);
+	colors[ImGuiCol_ButtonActive] = ImVec4(0.56f, 0.56f, 0.58f, 1.00f);
+	colors[ImGuiCol_Header] = ImVec4(0.26f, 0.59f, 0.98f, 0.31f);
+	colors[ImGuiCol_HeaderHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.80f);
+	colors[ImGuiCol_HeaderActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	colors[ImGuiCol_Separator] = ImVec4(0.43f, 0.43f, 0.50f, 0.50f);
+	colors[ImGuiCol_SeparatorHovered] = ImVec4(0.10f, 0.40f, 0.75f, 0.78f);
+	colors[ImGuiCol_SeparatorActive] = ImVec4(0.10f, 0.40f, 0.75f, 1.00f);
+	colors[ImGuiCol_ResizeGrip] = ImVec4(0.26f, 0.59f, 0.98f, 0.20f);
+	colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
+	colors[ImGuiCol_ResizeGripActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.95f);
+	colors[ImGuiCol_Tab] = ImVec4(0.18f, 0.35f, 0.58f, 0.86f);
+	colors[ImGuiCol_TabHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.80f);
+	colors[ImGuiCol_TabActive] = ImVec4(0.20f, 0.41f, 0.68f, 1.00f);
+	colors[ImGuiCol_TabUnfocused] = ImVec4(0.07f, 0.10f, 0.15f, 0.97f);
+	colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.14f, 0.26f, 0.42f, 1.00f);
+	colors[ImGuiCol_PlotLines] = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
+	colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
+	colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+	colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
+	colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
+	colors[ImGuiCol_DragDropTarget] = ImVec4(1.00f, 1.00f, 0.00f, 0.90f);
+	colors[ImGuiCol_NavHighlight] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.00f, 1.00f, 1.00f, 0.70f);
+	colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.20f);
+	colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.35f);
+
+	style.WindowRounding = 0.0f;
+	style.ChildRounding = 0.0f;
+	style.FrameRounding = 2.0f;
+	style.GrabRounding = 0.0f;
+	style.PopupRounding = 0.0f;
+	style.ScrollbarRounding = 0.0f;
+	style.TabRounding = 0.0f;
+	style.FramePadding.y = 5.0f;
+	style.ButtonTextAlign = ImVec2(0.5, 0.5);
+	
+	ImGui_ImplWin32_Init(main_hwnd);
+	ImGui_ImplDX9_Init(g_pd3dDevice);
+
+	DWORD window_flags = ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoResize;
+	RECT screen_rect;
+	GetWindowRect(GetDesktopWindow(), &screen_rect);
+	auto x = float(screen_rect.right - width) / 2.0f;
+	auto y = float(screen_rect.bottom - width) / 2.0f;
+
+	static int tabs = 2;
+	bool InfWindow = false;
+
+	KeyAuthApp.init();
+
+
+	MSG msg;
+	ZeroMemory(&msg, sizeof(msg));
+	while (msg.message != WM_QUIT && !LoginCheck) {
+		if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+			continue;
+		}
+		ImGui_ImplDX9_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+		{
+			ImGui::SetNextWindowPos(ImVec2(x, y), ImGuiCond_Once);
+			ImGui::SetNextWindowSize({ 401.f, 292.f });
+			ImGui::SetNextWindowBgAlpha(1.0f);
+
+			//please make the register via KeyAuth website because I'm lazy to make a register in the loader.
+			ImGui::Begin("Loader(Stable-Version)", &loader_active, window_flags);
+			{
+				if (tabs == 2) {
+					ImGui::Text("Please Login");
+
+					ImGui::Spacing();
+
+					ImGui::Text("Username:");
+
+					ImGui::Spacing();
+
+					ImGui::InputText(" ", UserName, IM_ARRAYSIZE(UserName));
+
+					ImGui::Spacing();
+
+					ImGui::Text("Password:");
+
+					ImGui::Spacing();
+
+					ImGui::InputText("", PassWord, IM_ARRAYSIZE(PassWord), ImGuiInputTextFlags_Password);
+
+					ImGui::Spacing();
+					ImGui::Spacing();
+
+					if (ImGui::Button("Login##Log", ImVec2(201, 27)))
+					{
+						KeyAuthApp.login(UserName, PassWord);
+						MessageBox(NULL, TEXT("Login successful!"), TEXT("Login"), MB_OK);
+						tabs = 2;
+						InfWindow = true;
+					}
+					ImGui::SetCursorPos({ 267.f, 270.f });
+					ImGui::Text("nesk#0001 (discord)");
+				}
+			}
+			ImGui::End();
+
+			if (InfWindow) {
+				InfLog();
+			}
+		}
+		ImGui::EndFrame();
+
+		g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 1.0f, 0, 0);
+		if (g_pd3dDevice->BeginScene() >= 0) {
+			ImGui::Render();
+			ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+			g_pd3dDevice->EndScene();
+		}
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+		}
+		HRESULT result = g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
+		if (result == D3DERR_DEVICELOST && g_pd3dDevice->TestCooperativeLevel() == D3DERR_DEVICENOTRESET) {
+			ResetDevice();
+		}
+		if (!loader_active) {
+			msg.message = WM_QUIT;
+		}
+	}
+	ImGui_ImplDX9_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+
+	UnregisterClass(wc.lpszClassName, wc.hInstance);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+		return true;
+	switch (msg)
+	{
+	case WM_SIZE:
+		if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+		{
+			g_d3dpp.BackBufferWidth = LOWORD(lParam);
+			g_d3dpp.BackBufferHeight = HIWORD(lParam);
+			ResetDevice();
+		}
+		return 0;
+	case WM_SYSCOMMAND:
+		if ((wParam & 0xfff0) == SC_KEYMENU)
+			return 0;
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
